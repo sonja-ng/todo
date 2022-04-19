273a3d38c22765a8081b26916d0c993445287a66
Sonja <sonja.ng1@gmail.com>
2022-04-19 15:07:06 -0400

    finish form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,27 @@ function Todo({todo}){
   );
 };
 
+function TodoForm({addTodo}){
+  const [ value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (value.length === 0) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+       className="input"
+       value={value}
+       onChange={e=> setValue(e.target.value)}/>
+      <button>Submit</button> 
+    </form>
+  )
+};
+
 function App() {
   const [todos, setTodos] = React.useState([
     { text: "learn react" },
@@ -16,11 +37,17 @@ function App() {
     { text: "build really cool app" }
   ])
 
+  const addTodo = (task) => {
+    const newTodos = [...todos, { text: task }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
       {todos.map((todo, idx) => <Todo key={idx} index={idx} todo={todo}/>)}
         </div>
+        <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
